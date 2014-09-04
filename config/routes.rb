cef58d848610f2edadff28710448a8de2296701c
Greg Stanring <standringg@gmail.com>
2014-09-04 16:39:48 +1200

    Ajax working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 TotDepot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'products#index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
