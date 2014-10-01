b97cdec6162abdb54f0fa4ea2dce1a0205ef6a0d
Greg Stanring <standringg@gmail.com>
2014-10-01 16:50:56 +1300

    updated actionmailer config

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,29 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 TotDepot::Application.initialize!
 
-TotDepot::Application.configure do 
-  config.action_mailer.delivery_method = :smtp
+#config for using sendgrid
 
-  config.action_mailer.smtp_settings = { 
-  	address: "smtp.sendgrid.net", 
-  	port: 587, 
-  	domain: "app29208050@heroku.com", 
-  	authentication: "plain", 
-  	user_name: "app29208050@heroku.com", 
-  	password: "6xici22o", 
-  	enable_starttls_auto: true 
-  } 
-end
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app29208050@heroku.com",
+  :password => "6xici22o",
+  :domain => "touroftauranga@gmail.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+ #TotDepot::Application.configure do
+  #config.action_mailer.delivery_method = :smtp
+
+  #config.action_mailer.smtp_settings = { 
+  	#address: "smtp.sendgrid.net", 
+  	#port: 587, 
+  	#domain: "touroftauranga@gmail.com", 
+  	#authentication: "plain", 
+  	#user_name: "app29208050@heroku.com", 
+  	#password: "6xici22o", 
+  	#enable_starttls_auto: true 
+ # } 
+#end
